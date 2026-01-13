10a0bea1fcdcfe1bf7eaf01dc08f82f18a155da7
Musfika Rahman Ananna <58202644+anannatithi@users.noreply.github.com>
2026-01-14 01:12:25 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  // Required for GitHub Pages (Project Pages)
-  basePath: "/portfolio-website",
-
-  // CRITICAL: ensures index.html generation
   trailingSlash: true,
-
   images: {
     unoptimized: true,
   },
